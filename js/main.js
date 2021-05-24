6a80da2e568aeabe5a2b228dd0aa5e3b7c9a9e79
cassiopeia <cassiopeia.neely@gmail.com>
2021-05-23 23:02:53 -0400

    create blankAlert() function
    Created an alert that will prompt the user to enter text, if they make a dropdown selection while the textarea is empty.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
 $(document).ready(function() {
+    
+    // if the user makes a selection
+    function blankAlert() {
+        // and the textarea is empty
+        if ($('textarea').val('')) {
+            // prompt the user to input text
+            alert('Add some text!');
+        }
+    };
+
     // if the lowercase option is selected
     $('#lowercase').on('click', function() {
         // convert all textarea content to lowercase
         $('textarea').val(function() {
             return this.value.toLowerCase();
         });
+
+        blankAlert();
     });
 
     // if the uppercase option is selected
@@ -13,6 +25,8 @@ $(document).ready(function() {
         $('textarea').val(function() {
             return this.value.toUpperCase();
         }); 
+
+        blankAlert();
     });
 });
 
