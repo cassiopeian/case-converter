6e4e2d95bee113a86767dba35e722291d0a9b933
cassiopeia <cassiopeia.neely@gmail.com>
2021-05-23 22:47:27 -0400

    create #uppercase on click
    If the user selects the "uppercase" dropdown option, the textarea content will be converted to uppercase.

--- js/main.js
+++ js/main.js
@@ -6,5 +6,13 @@ $(document).ready(function() {
             return this.value.toLowerCase();
         });
     });
+
+    // if the uppercase option is selected
+    $('#uppercase').on('click', function() {
+        // convert all textarea content to uppercase
+        $('textarea').val(function() {
+            return this.value.toUpperCase();
+        }); 
+    });
 });
 
