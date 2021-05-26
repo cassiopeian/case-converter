118993310d543ece4f7472d4e8156d211782de20
cassiopeia <cassiopeia.neely@gmail.com>
2021-05-25 22:52:16 -0400

    add rotateArrow() and resolve bubbling issue
    » Created a function, rotateArrow(), that rotates the placeholder arrow, based on the display state of the dropdown
    » Created a #selection-placeholder on click, which calls rotateArrow() and toggles #dropdown
    » Updated the #lowercase and #uppercase on clicks, so they also call rotateArrow() and ensure the dropdown is closed, when a selection is made. I also added event.stopPropagation() to both, since event bubbling caused issues with the #dropdown display and the arrow rotation.

--- js/main.js
+++ js/main.js
@@ -9,22 +9,56 @@ $(document).ready(function() {
         }
     };
 
+    function rotateArrow() {
+        // if the dropdown is visible
+        if ($('#dropdown').css('display') === 'block') {
+            // rotate the arrow, so it points up
+            $('#arrow').css({'transform': 'rotate(45deg)', 'margin': '0 0 5px 0'});
+        } else {
+            // rotate the arrow, so it points down
+            $('#arrow').css({'transform': 'rotate(225deg)', 'margin': '0'});
+        }
+    };
+
+    $('#selection-placeholder').on('click', function() {
+        rotateArrow();
+
+        // toggle dropdown display
+        $('#dropdown').toggle();
+    });
+
     // if the lowercase option is selected
-    $('#lowercase').on('click', function() {
+    $('#lowercase').on('click', function(event) {
         // convert all textarea content to lowercase
         $('textarea').val(function() {
             return this.value.toLowerCase();
         });
 
+        // prevent event bubbling
+        event.stopPropagation();
+
+        rotateArrow();
+
+        // close the dropdown
+        $('#dropdown').css('display', 'none');
+        
         blankAlert();
     });
 
     // if the uppercase option is selected
-    $('#uppercase').on('click', function() {
+    $('#uppercase').on('click', function(event) {
         // convert all textarea content to uppercase
         $('textarea').val(function() {
             return this.value.toUpperCase();
         }); 
+
+        // prevent event bubbling
+        event.stopPropagation();
+
+        rotateArrow();
+
+        // close the dropdown
+        $('#dropdown').css('display', 'none');
 
         blankAlert();
     });
