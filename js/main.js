dd6cb9215d8912b9e4b346fd435bdfa5ab83e830
cassiopeia <65559770+cassiopeian@users.noreply.github.com>
2022-01-08 22:51:06 -0500

    update up loop
    Added the phrase "bring down."

--- js/main.js
+++ js/main.js
@@ -270,7 +270,8 @@ $(document).ready(function() {
                     let leader = titleCased[up-1];
                 
                     // if the wanted word is part of a phrasal verb
-                    if (leader == 'Bring' && titleCased[up] == 'down' || 
+                    if (leader == 'Bring' && titleCased[up] == 'down' ||
+                        leader == 'Clean' && titleCased[up] == 'up' || 
                         leader == 'Hold' && titleCased[up] == 'on' ||
                         leader == 'Look' && titleCased[up] == 'up' ||
                         leader == 'Look' && titleCased[up] == 'down') {
